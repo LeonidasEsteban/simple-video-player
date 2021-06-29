597edfbb973d9de287879e0c966475d82b576042
Leonidas Esteban <leonidasesteban@gmail.com>
2021-06-29 13:45:48 -0500

    clase 9 - 10

--- js/index.js
+++ js/index.js
@@ -36,3 +36,24 @@ function handleForward() {
   $video.currentTime = $video.currentTime + 10
   console.log('para adelante 10 segundos', $video.currentTime)
 }
+
+const $progress = document.querySelector('#progress')
+$video.addEventListener('loadedmetadata', handleLoaded)
+$video.addEventListener('timeupdate', handleTimeUpdate)
+
+function handleLoaded() {
+  $progress.max = $video.duration
+  console.log('ha cargado mi video', $video.duration)
+}
+
+function handleTimeUpdate() {
+  $progress.value = $video.currentTime
+  // console.log('tiempo actual', $video.currentTime)
+}
+
+$progress.addEventListener('input', handleInput)
+
+function handleInput() {
+  $video.currentTime = $progress.value
+  console.log($progress.value)
+}
